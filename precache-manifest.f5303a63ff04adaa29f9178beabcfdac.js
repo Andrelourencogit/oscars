3c162ec6fc454a9a829cb073595f079dcfd92cd6
andré <andrelourenco@info.ufrn.br>
2022-02-02 17:23:17 -0300

    atualiza para criar pagina no git

---
+++ precache-manifest.f5303a63ff04adaa29f9178beabcfdac.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e799df8de56e2b94b6",
+    "url": "/the-oscar-teste/css/app.932b9fde.css"
+  },
+  {
+    "revision": "e561c6686a01689f3447",
+    "url": "/the-oscar-teste/css/chunk-vendors.8bda0bd5.css"
+  },
+  {
+    "revision": "8a9f1f2e710eb3c60f4e5ec49201bec3",
+    "url": "/the-oscar-teste/img/Logo-Oscars.8a9f1f2e.svg"
+  },
+  {
+    "revision": "e9e7ede91765d86600adf07e493545d9",
+    "url": "/the-oscar-teste/img/another.e9e7ede9.png"
+  },
+  {
+    "revision": "e020275d3697c5269bc26a1b8302e888",
+    "url": "/the-oscar-teste/img/bacurau.e020275d.png"
+  },
+  {
+    "revision": "9d405fdffbb450ba91a0f253a22d5f9f",
+    "url": "/the-oscar-teste/img/ceu.9d405fdf.png"
+  },
+  {
+    "revision": "07e13ca726558efe3b6191bf26769516",
+    "url": "/the-oscar-teste/img/chicago.07e13ca7.png"
+  },
+  {
+    "revision": "9a4278393fdf1e16d7e0de4489b18b1d",
+    "url": "/the-oscar-teste/img/emma.9a427839.png"
+  },
+  {
+    "revision": "06752f0a9bb519609f8284edaee28870",
+    "url": "/the-oscar-teste/img/fences.06752f0a.png"
+  },
+  {
+    "revision": "c03f63f13c27a50029b5a19389bdc226",
+    "url": "/the-oscar-teste/img/love.c03f63f1.png"
+  },
+  {
+    "revision": "7bf98856274fbe2bef1cd66e43398e7b",
+    "url": "/the-oscar-teste/img/moon.7bf98856.png"
+  },
+  {
+    "revision": "e4a2c314e972cc4bbe58256bd8f78e72",
+    "url": "/the-oscar-teste/img/mulan.e4a2c314.png"
+  },
+  {
+    "revision": "03a3ccaf62ff3333ad8177882bf0a0a5",
+    "url": "/the-oscar-teste/img/net.03a3ccaf.png"
+  },
+  {
+    "revision": "0a71dfc74f0127a1117ce8cde3aaff9d",
+    "url": "/the-oscar-teste/index.html"
+  },
+  {
+    "revision": "62e799df8de56e2b94b6",
+    "url": "/the-oscar-teste/js/app.305655b1.js"
+  },
+  {
+    "revision": "e561c6686a01689f3447",
+    "url": "/the-oscar-teste/js/chunk-vendors.d639430d.js"
+  },
+  {
+    "revision": "6a453355fb59d6a83b2f6398e829802f",
+    "url": "/the-oscar-teste/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/the-oscar-teste/robots.txt"
+  }
+]);
